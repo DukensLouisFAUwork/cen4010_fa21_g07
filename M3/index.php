7c3e6a365b83f60ece58a1eba856fe3b23ee8229
Schukl21 <schukluke@gmail.com>
2021-10-25 19:40:50 -0400

    updated functionality

--- M3/index.php
+++ M3/index.php
@@ -6,79 +6,30 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<?php
-$_SESSION["user_id"] = "1";
-echo $_SESSION['user_id'];
-echo "Session variables are set.";
-$sql = "SELECT id, user_id, caption,img FROM posts ";
-$result = $conn->query($sql);
-$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+     <form action="login.php" method="post">
+     	<h2>LOGIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>User Name</label>
+         <label>
+             <input type="text" name="uname" placeholder="User Name">
+         </label><br>
 
-if(isset($_POST["submit"])&& $_POST['randcheck']==$_SESSION['rand']){
-    $name = $_FILES['image']['name'];
-    $imgData = addslashes(file_get_contents($_FILES['image']['tmp_name']));
-    $upload = "INSERT INTO posts (user_id, caption, img, like1, like2, like3, like_total) VALUES('$_SESSION[user_id]','$_POST[caption]','$imgData ',0,0,0,0 )";
-    $hope = $conn->query($upload);
-    if($hope)
-    {
-        echo '<script type="text/javascript"> alert("Data Inserted Seccessfully!"); </script>';  // alert message
-    }
-    else
-    {
-        echo '<script type="text/javascript"> alert("Error Uploading Data!"); </script>';  // when error occur
-    }
-}
-?>
+     	<label>Password</label>
+         <label>
+             <input type="password" name="password" placeholder="Password">
+         </label><br>
 
-<?php
-function test($conn){
-    $sql1 = "UPDATE posts SET like1=like1 + 1 WHERE id = 1 ";
-    $conn->query($sql1);
-    if ($conn->query($sql1) === TRUE) {
-        echo "Record updated successfully";
-    } else {
-        echo "Error updating record: " . $conn->error;
-    }
-}
-?>
-
-<?php foreach($posts as $post){ ?>
-    <br><a href="profile.php?name=<?php echo $post['user_id'] ?>">user's profile page</a>
-    <br><?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $post['img'] ).'"/>';?>
-    <br><?php echo htmlspecialchars($post['caption']); ?>
-    <br><button onclick = "">1st like</button>
-    <button>2nd like</button>
-    <button>3rd like</button>
-
-
-<?php } ?>
-<form method="post" enctype="multipart/form-data">
-    <?php
-    $rand=rand();
-    $_SESSION['rand']=$rand;?>
-    <input type="hidden" value="<?php echo $rand; ?>" name="randcheck" />
-
-    <table>
-        <tr>
-            <td>Select Image</td>
-            <td><input type="file" name="image" Required></td>
-        </tr>
-        <tr>
-            <td>Enter Caption</td>
-            <td><input type="text" name="caption" placeholder="Enter Caption" Required></td>
-        </tr>
-        <tr>
-            <td colspan="2"><input type="submit" name="submit" value="Upload"></td>
-        </tr>
-    </table>
-</form>
+     	<button type="submit">Login</button>
+          <a href="signup.php" class="ca">Create an account</a>
+     </form>
 </body>
 </html>
